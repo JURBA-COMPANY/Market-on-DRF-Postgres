42a9d4989e199d65d65f89a609422e5a11382612
Kprdt <90186481+Kiprdt@users.noreply.github.com>
2023-03-15 14:43:42 +0300

    Update stol_service.py

--- База данных/stol_service.py
+++ База данных/stol_service.py
@@ -2,7 +2,12 @@
 
 from service_parent import service
 
+
 class stol_service(service):
     def get(self, idles):
         getid = session.query(Stol.id, Stol.expand).filter_by(id=idles).all()
         return getid
+    
+    def getall(self):
+        getidall = session.query(Stol.id, Stol.expand).all()
+        return getidall
