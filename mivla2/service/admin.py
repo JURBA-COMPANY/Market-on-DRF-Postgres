44be188b416512a7bd1cf33906aa2f871eb22678
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2023-04-02 23:54:44 +0300

    Update admin.py
    Register all models

--- mivla2/service/admin.py
+++ mivla2/service/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Sofa)
+admin.site.register(Users)
+admin.site.register(Wardrobe)
+admin.site.register(Stol)
+admin.site.register(Orders)
+admin.site.register(Colours)
+admin.site.register(Furniture)
+admin.site.register(Shipment)
+admin.site.register(Diler)
