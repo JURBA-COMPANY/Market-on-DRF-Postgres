640e16a31a43a70f1b22f97b226b3f2b5fdf63fc
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2023-04-02 23:54:21 +0300

    Update models.py
    Rename all Foreign key columns

--- mivla2/service/models.py
+++ mivla2/service/models.py
@@ -6,16 +6,18 @@ class Users(models.Model):
         db_table = 'users'
         managed = False
     user_id = models.BigAutoField(primary_key=True)
-    name = models.CharField(null=False, max_length=50),
+    name = models.CharField(null=False, max_length=50)
     second_name = models.CharField(null=False, max_length=50)
     phone = models.CharField(null=False, max_length=12)
     email = models.CharField(null=False, max_length=100)
+
+    def __str__(self):
+        return f"{self.name} {self.second_name}"
 
 
 class Colours(models.Model):
     class Meta:
         db_table = 'colours'
-        managed = False
     colour_id = models.BigAutoField(primary_key=True)
     colour_name = models.CharField(null=False, max_length=30)
 
@@ -33,8 +35,8 @@ class Shipment(models.Model):
         db_table = 'shipment'
     ship_id = models.BigAutoField(primary_key=True)
     ship_date = models.DateField(null=False)
-    weigth = models.FloatField(max_length=30)
-    diler_id = models.ForeignKey(to='Diler', on_delete=models.CASCADE)
+    weight = models.FloatField(max_length=30)
+    diler = models.ForeignKey(to='Diler', on_delete=models.CASCADE)
 
 
 class Furniture(models.Model):
@@ -44,8 +46,8 @@ class Furniture(models.Model):
     length = models.FloatField(max_length=20, null=False, blank=False)
     height = models.FloatField(max_length=20, null=False, blank=False)
     depth = models.FloatField(max_length=20, null=False, blank=False)
-    ship_name = models.ForeignKey(to='Shipment', on_delete=models.CASCADE)
-    colour_id = models.ForeignKey(to='Colours', on_delete=models.CASCADE)
+    ship = models.ForeignKey(to='Shipment', on_delete=models.CASCADE)
+    colour = models.ForeignKey(to='Colours', on_delete=models.CASCADE)
 
 
 class Sofa(models.Model):
@@ -53,10 +55,16 @@ class Sofa(models.Model):
         db_table = 'sofa'
 
     self_id = models.AutoField(primary_key=True)
-    fur_id = models.ForeignKey(to='Furniture', on_delete=models.PROTECT)
+    fur = models.ForeignKey(to='Furniture', on_delete=models.PROTECT)
     num_seats = models.IntegerField(null=False, blank=False)
     expand = models.BooleanField(null=False)
     corner = models.BooleanField(null=False)
+
+    def __str__(self):
+        if self.expand:
+            return f"Диван угловой {self.self_id}"
+        else:
+            return f"Диван {self.self_id}"
 
 
 class Wardrobe(models.Model):
@@ -64,7 +72,7 @@ class Wardrobe(models.Model):
         db_table = 'wardrobe'
 
     self_id = models.AutoField(primary_key=True)
-    fur_id = models.ForeignKey(to='Furniture', on_delete=models.PROTECT)
+    fur = models.ForeignKey(to='Furniture', on_delete=models.PROTECT)
     num_shelfs = models.IntegerField(null=False, blank=False)
 
 
@@ -73,7 +81,7 @@ class Stol(models.Model):
         db_table = 'stol'
 
     self_id = models.AutoField(primary_key=True)
-    fur_id = models.ForeignKey(to='Furniture', on_delete=models.PROTECT)
+    fur = models.ForeignKey(to='Furniture', on_delete=models.PROTECT)
     expand = models.BooleanField(null=False)
 
 
@@ -81,6 +89,7 @@ class Orders(models.Model):
     class Meta:
         db_table = 'orders'
 
-    user_id = models.ForeignKey(to='Users', on_delete=models.PROTECT)
+    user = models.ForeignKey(to='Users', on_delete=models.PROTECT)
     order_date = models.DateField(null=False)
     order_cost = models.FloatField(max_length=15, null=False)
+
