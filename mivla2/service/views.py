e80616a661173cb93dbb2b7cf659b87bf118c4ca
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2023-04-08 08:29:37 +0300

    Update views.py

--- mivla2/service/views.py
+++ mivla2/service/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .parent import get_all, get
 
-# Create your views here.
+
+def main_page(request):
+    return render(request, template_name='service/index.html')
+
+
+def view_get_all(request):
+    context = {"entities": get_all()}
+    return render(request, template_name='service/catalog.html', context=context)
+
