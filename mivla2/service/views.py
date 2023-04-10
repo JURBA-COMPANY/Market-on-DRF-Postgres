83fa1cfcaa04a95675b4323e5c0652b3cbd9233f
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2023-04-10 23:19:03 +0300

    Update views.py

--- mivla2/service/views.py
+++ mivla2/service/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from .parent import get_all, get
+from .parent import Service
+from .models import *
 
 
-def main_page(request):
-    return render(request, template_name='service/index.html')
-
-
-def view_get_all(request):
-    context = {"entities": get_all()}
-    return render(request, template_name='service/catalog.html', context=context)
-
+def shop(request):
+    product = Service()
+    lister = product.get_all(Sofa, Wardrobe, Stol)
+    context = {'items': lister}
+    return render(request, 'service/catalog.html', context)
