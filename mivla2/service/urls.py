34c2a3ea96e1a52ccf1ed21fa43187f645d659eb
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2023-04-12 12:34:23 +0300

    Update urls.py

--- mivla2/service/urls.py
+++ mivla2/service/urls.py
@@ -4,7 +4,7 @@ from service.views import *
 app_name = 'service'
 
 urlpatterns = [
-    path('catalog', Service().get_all(Sofa, Stol, Wardrobe), name='catalog'),
+    path('catalog', shop, name='catalog'),
     path('api/v1/sofalist', SofaAPIView.as_view()),
     path('api/v1/wardrobelist', WardrobeAPIView.as_view()),
     path('api/v1/stollist', StolAPIView.as_view()),
