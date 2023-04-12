6a8b9b02b01896c46d492e42638d8c680d4c1d8d
Kprdt <90186481+Kiprdt@users.noreply.github.com>
2023-04-12 11:09:16 +0300

    Update views.py

--- mivla2/service/views.py
+++ mivla2/service/views.py
@@ -8,3 +8,23 @@ def shop(request):
     lister = product.get_all(Sofa, Wardrobe, Stol)
     context = {'items': lister}
     return render(request, 'service/catalog.html', context)
+
+
+class SofaAPIView(generics.ListAPIView):
+    queryset = Sofa.objects.all()
+    serializer_class = SofaSerializer
+
+
+class StolAPIView(generics.ListAPIView):
+    queryset = Stol.objects.all()
+    serializer_class = StolSerializer
+
+
+class WardrobeAPIView(generics.ListAPIView):
+    queryset = Wardrobe.objects.all()
+    serializer_class = WardrobeSerializer
+    
+    
+class FurnitureAPIView(generics.ListAPIView):
+    queryset = Furniture.objects.all()
+    serializer_class = FurnitureSerializer
