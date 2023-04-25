f260de8dd89a4ccb2678d6e900512c32a1e6e9e7
Kprdt <90186481+Kiprdt@users.noreply.github.com>
2023-04-26 00:16:15 +0300

    Update serializers.py

--- mivla2/service/serializers.py
+++ mivla2/service/serializers.py
@@ -2,25 +2,44 @@ from rest_framework import serializers
 from .models import *
 
 
+
+class ColoursSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Colours
+        fields = ('colour_name',)
+        
+        
 class FurnitureSerializer(serializers.ModelSerializer):
+    
+    colour = ColoursSerializer()
+    
     class Meta:
         model = Furniture
         fields = ('length', 'height', 'depth', 'ship', 'colour')
 
 
 class SofaSerializer(serializers.ModelSerializer):
+    
+    fur = FurnitureSerializer()
+    
     class Meta:
         model = Sofa
         fields = ('self_id', 'fur', 'num_seats', 'expand', 'corner')
 
 
 class StolSerializer(serializers.ModelSerializer):
+    
+    fur = FurnitureSerializer()
+    
     class Meta:
         model = Stol
         fields = ('self_id', 'fur', 'expand')
 
 
 class WardrobeSerializer(serializers.ModelSerializer):
+    
+    fur = FurnitureSerializer()
+    
     class Meta:
         model = Wardrobe
         fields = ('self_id', 'fur', 'num_shelfs')
