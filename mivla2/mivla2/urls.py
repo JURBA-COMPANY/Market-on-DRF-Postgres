58e0b4245891620ca3f6c736aeb5e9cb6bfc4afd
Kprdt <90186481+Kiprdt@users.noreply.github.com>
2023-05-16 20:25:42 +0300

    Update urls.py

--- mivla2/mivla2/urls.py
+++ mivla2/mivla2/urls.py
@@ -15,14 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from service.views import *
+
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainpage.urls'), name='index'),
-    path('catalog/', include('service.urls'), name='get_all')
+    path('catalog/', include('service.urls'), name='get_all'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
 
 
