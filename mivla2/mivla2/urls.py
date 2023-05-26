543ec7ca9f6dc42ef8239127102c1d6d42e620ff
Kprdt <90186481+Kiprdt@users.noreply.github.com>
2023-05-26 13:36:57 +0300

    Update urls.py

--- mivla2/mivla2/urls.py
+++ mivla2/mivla2/urls.py
@@ -1,33 +1,17 @@
-"""mivla2 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from service.views import *
 
-
+from services.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainpage.urls'), name='index'),
-    path('catalog/', include('service.urls'), name='get_all'),
+    path('catalog/', include('services.urls', namespace='services'), name='get_all'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
-]
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
+]
 
