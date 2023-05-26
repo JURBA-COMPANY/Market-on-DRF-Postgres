a28ac8f91e7a9f0a73924fba5525f0f6044ed0a4
Kprdt <90186481+Kiprdt@users.noreply.github.com>
2023-05-26 13:35:11 +0300

    Update serializers.py

--- mivla2/service/serializers.py
+++ mivla2/service/serializers.py
@@ -1,45 +1,168 @@
+from django.contrib import auth
 from rest_framework import serializers
-from .models import *
+from services.models import Furniture, Sofa, Stol, Wardrobe, Colours, Orders, User
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
+
+class UserSerializer(serializers.ModelSerializer):
+    """ Ощуществляет сериализацию и десериализацию объектов User. """
+
+    # Пароль должен содержать от 8 до 128 символов. Это стандартное правило. Мы
+    # могли бы переопределить это по-своему, но это создаст лишнюю работу для
+    # нас, не добавляя реальных преимуществ, потому оставим все как есть.
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password')
+
+        # Параметр read_only_fields является альтернативой явному указанию поля
+        # с помощью read_only = True, как мы это делали для пароля выше.
+        # Причина, по которой мы хотим использовать здесь 'read_only_fields'
+        # состоит в том, что нам не нужно ничего указывать о поле. В поле
+        # пароля требуются свойства min_length и max_length,
+        # но это не относится к полю токена.
+        read_only_fields = ('token',)
+
+    def update(self, instance, validated_data):
+        """ Выполняет обновление User. """
+
+        # В отличие от других полей, пароли не следует обрабатывать с помощью
+        # setattr. Django предоставляет функцию, которая обрабатывает пароли
+        # хешированием и 'солением'. Это означает, что нам нужно удалить поле
+        # пароля из словаря 'validated_data' перед его использованием далее.
+        password = validated_data.pop('password', None)
+
+        for key, value in validated_data.items():
+            # Для ключей, оставшихся в validated_data мы устанавливаем значения
+            # в текущий экземпляр User по одному.
+            setattr(instance, key, value)
+
+        if password is not None:
+            # 'set_password()' решает все вопросы, связанные с безопасностью
+            # при обновлении пароля, потому нам не нужно беспокоиться об этом.
+            instance.set_password(password)
+
+        # После того, как все было обновлено, мы должны сохранить наш экземпляр
+        # User. Стоит отметить, что set_password() не сохраняет модель.
+        instance.save()
+
+        return instance
 
 
 class ColoursSerializer(serializers.ModelSerializer):
     class Meta:
         model = Colours
         fields = ('colour_name',)
-        
-        
+
+
 class FurnitureSerializer(serializers.ModelSerializer):
-    
+
     colour = ColoursSerializer()
-    
+
     class Meta:
         model = Furniture
-        fields = ('length', 'height', 'depth', 'ship', 'colour')
+        fields = ('id', 'length', 'height', 'depth', 'ship', 'colour')
 
 
 class SofaSerializer(serializers.ModelSerializer):
-    
+
     fur = FurnitureSerializer()
-    
+
     class Meta:
         model = Sofa
         fields = ('self_id', 'fur', 'num_seats', 'expand', 'corner')
 
 
 class StolSerializer(serializers.ModelSerializer):
-    
-    fur = FurnitureSerializer()
-    
+
+    fur = FurnitureSerializer(read_only=True)
+
     class Meta:
         model = Stol
         fields = ('self_id', 'fur', 'expand')
 
 
 class WardrobeSerializer(serializers.ModelSerializer):
-    
-    fur = FurnitureSerializer()
-    
+
+    fur = FurnitureSerializer(read_only=True)
+
     class Meta:
         model = Wardrobe
         fields = ('self_id', 'fur', 'num_shelfs')
+
+
+class OrdersSerializer(serializers.ModelSerializer):
+
+    product = FurnitureSerializer(read_only=True)
+    user = UserSerializer()
+
+    class Meta:
+        model = Orders
+        fields = ("user", "order_date", "order_cost", "product")
+
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+        if not username.isalnum():
+            raise serializers.ValidationError(
+                self.default_error_messages)
+        return attrs
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(max_length=255, min_length=3)
+    password = serializers.CharField(max_length=68, min_length=6,write_only=True)
+    tokens = serializers.SerializerMethodField()
+    def get_tokens(self, obj):
+        user = User.objects.get(username=obj['username'])
+        return {
+            'refresh': user.tokens()['refresh'],
+            'access': user.tokens()['access']
+        }
+    class Meta:
+        model = User
+        fields = ['email', 'password','tokens']
+    def validate(self, attrs):
+        email = attrs.get('email','')
+        password = attrs.get('password','')
+        user = auth.authenticate(email=email,password=password)
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin')
+        return {
+            'email': user.email,
+            'username': user.username,
+            'tokens': user.tokens
+        }
+
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self, **kwargs):
+        try:
+            RefreshToken(self.token).blacklist()
+        except TokenError:
+            self.fail('bad_token')
