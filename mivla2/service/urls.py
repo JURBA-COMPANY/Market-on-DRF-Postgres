292a3f7acf010e037df2dd2480b89c39dfc25de4
Kprdt <90186481+Kiprdt@users.noreply.github.com>
2023-05-26 13:36:33 +0300

    Update urls.py

--- mivla2/service/urls.py
+++ mivla2/service/urls.py
@@ -1,13 +1,23 @@
-from django.urls import path
-from service.views import *
+from django.urls import path, include
+from rest_framework import routers
 
-app_name = 'service'
+
+from services.views import *
+
+app_name = 'services'
+router = routers.SimpleRouter()
+router.register(r'sofa', SofaViewSet)
+router.register(r'stol', StolViewSet)
+router.register(r'wardrobe', WardrobeViewSet)
+router.register(r'furniture', FurnitureViewSet)
+router.register(r'orders', OrdersViewSet)
+router.register(r'user', UserViewSet)
+
 
 urlpatterns = [
     path('', shop, name='catalog'),
-    path('api/v1/sofalist', SofaAPIView.as_view()),
-    path('api/v1/wardrobelist', WardrobeAPIView.as_view()),
-    path('api/v1/stollist', StolAPIView.as_view()),
-    path('api/v1/furniturelist', FurnitureAPIView.as_view()),
-    path('api/v1/sofalist/<int:pk>', SofaEntityView.as_view())
+    path('api/v1/', include(router.urls)),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('login/', LoginAPIView.as_view(), name="login"),
+    path('logout/', LogoutAPIView.as_view(), name="logout"),
 ]
